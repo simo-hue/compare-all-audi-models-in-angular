c48228dbce9325e0830a2352957760b54c32b10a
MatioliSimone <simone.mattioli@marconirovereto.it>
2021-05-12 11:15:04 +0200

    statoService

--- src/app/common/stato.service.ts
+++ src/app/common/stato.service.ts
@@ -1,35 +1,28 @@
 import { Injectable } from '@angular/core';
+import { Modello } from './modello.model';
 
 @Injectable()
 export class StatoService {
   // se far partire il confronto tra due veicoli o no
   confronto: boolean;
-  contatore: number;
+  contatore: Array<Modello>;
   constructor() {
     this.confronto = false;
-    this.contatore = 0;
-   }
+    this.contatore = Array(0);
+  }
+  aggiungiConfronto(modello: Modello) {
+    this.contatore.push(modello);
+  }
+  rimuoviConfronto(modello: Modello) {
+    let i = this.contatore.indexOf(modello);
+    if (i < this.contatore.length) this.contatore.splice(i, 1);
+  }
+  getContatore() {
+    return this.contatore;
+  }
 
-  getConfronto(): boolean{
-     return this.confronto;
-   }
-
-   incContatore(){
-     this.contatore +=1;
-     return this.contatore;
-   }
-
-   decContatore(){
-     this.contatore -=1;
-     return this.contatore;
-   }
-
-   getContatore(){
-     return this.contatore;
-   }
-
-   setContatoreTrue(): boolean{
-     this.confronto == true;
-     return this.confronto;
-   }
+  setContatoreTrue(): boolean {
+    this.confronto == true;
+    return this.confronto;
+  }
 }
